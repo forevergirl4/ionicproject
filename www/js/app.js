29768277e5aa7e1ff218f6f9b522b46c873ac37d
Emarie <emariecarbajosa@gmail.com>
2017-01-31 23:16:46 +0800

    declaring controller to view

--- www/js/app.js
+++ www/js/app.js
@@ -24,7 +24,7 @@ angular.module('starter', ['ionic'])
 })
 .controller('ListController', ['$scope', '$http', function($scope, $http){
   $http.get('js/data.json').success(function(data){
-    $scope.artists = data;
+    $scope.artists = data.artists;
 
   });
 }]);
