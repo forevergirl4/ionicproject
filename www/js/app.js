2fbcb645378f9d80024ae627cbe906e53475de07
Emarie <emariecarbajosa@gmail.com>
2017-02-01 09:43:45 +0800

    Working on multiple buttons

--- www/js/app.js
+++ www/js/app.js
@@ -89,26 +89,25 @@ angular.module('starter', ['ionic'])
       $scope.artists.splice(toIndex, 0, item)
     }
   })
-}]).controller('CalendarController', ['$scope', '$http', '$state', function($scope, $http, $state){
-  $http.get('js/data.json').success(function(data){
-    $scope.calendar = data.calendar;
+}]).controller('CalendarController', ['$scope', '$http', '$state',
+    function($scope, $http, $state) {
+    $http.get('js/data.json').success(function(data) {
+      $scope.calendar = data.calendar;
 
-    $scope.doRefresh = function(){
-      $http.get('js/data.json').success(function(data){
-        $scope.calendar = data.calendar;
-        $scope.$broadcast('scroll.refreshComplete');
-      })
-    }
-    $scope.onItemDelete = function(item){
-      $scope.calendar.splice($scope.calendar.indexOf(item), 1);
-    };
+      $scope.onItemDelete = function(dayIndex, item) {
+        $scope.calendar[dayIndex].schedule.splice($scope.calendar[dayIndex].schedule.indexOf(item), 1);
+      }
 
-    $scope.toggleStar = function(item){
-      item.star = !item.star;
-    }
-    $scope.moveItem = function(item, fromIndex, toIndex){
-      $scope.calendar.splice(fromIndex, 1);
-      $scope.calendar.splice(toIndex, 0, item)
-    }
-  })
+      $scope.doRefresh =function() {
+      $http.get('js/data.json').success(function(data) {
+          $scope.calendar = data.calendar;
+          $scope.$broadcast('scroll.refreshComplete');
+        });
+      }
+
+      $scope.toggleStar = function(item) {
+        item.star = !item.star;
+      }
+
+    });
 }]);
